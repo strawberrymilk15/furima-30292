8db145294fd0153880e147d18f34fbb3136cd9ae
strawberrymilk15 <69702033+strawberrymilk15@users.noreply.github.com>
2021-02-15 22:42:33 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :log_user_edit, only: :edit
-  before_action :authenticate_user!, only: [:new,:edit]
+  before_action :itemmer
+  before_action :authenticate_user!, except: [:index,:show]
 
   def index
     @items = Item.order("created_at DESC").includes(:user)
@@ -12,7 +13,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @saves=if @item.save
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -48,9 +47,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name,:price,:description_of_item,:prefecture_id,:product_condition_id,:date_of_shipment_id,:shipping_charge_id,:category_id,:image).merge(user_id: current_user.id)
   end
 
+  def itemmer 
+    @item = Item.find(params[:id])
+  end
+
 
   def log_user_edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
