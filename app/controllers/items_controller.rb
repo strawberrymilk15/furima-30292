85f71f82bb27cb60e291f6ee6d1e2716f44dddc7
strawberrymilk15 <69702033+strawberrymilk15@users.noreply.github.com>
2021-02-16 13:43:42 +0900

    修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :log_user_edit, only: [:edit,:update]
-  before_action :set_item
+  before_action :set_item, only: [:show,:edit,:update]
   before_action :authenticate_user!, except: [:index,:show]
 
   def index
