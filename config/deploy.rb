c594098c555b9853b775b0212fcacd67465bf6c2
strawberrymilk15 <69702033+strawberrymilk15@users.noreply.github.com>
2021-02-21 19:29:27 +0900

    update config /deploy.rb / enviroments/production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'furima-30292'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:strawberrymlk15/furima-30292.git'
+set :repo_url,  'git@github.com:strawberrymilk15/furima-30292.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
